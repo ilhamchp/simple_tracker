9ee9a5e7234eee619cba30fdbf28c708c1151e7b
Ilham Cahyahadi <ilham.cahyahadi@aksesciptasolusi.com>
2022-02-11 17:12:25 +0700

    Add google maps sdk

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      
+      GMSServices.provideAPIKey("AIzaSyDsol1tjzTS_mpuxfRaVIeRslJjY5g3I4I")
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- lib/widgets/coordinate_widget.dart
+++ lib/widgets/coordinate_widget.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:simple_tracker/providers/CoordinateProvider.dart';
 import 'package:simple_tracker/providers/timer_provider.dart';
@@ -14,23 +17,44 @@ class CoordinateWidget extends StatefulWidget {
 }
 
 class _CoordinateWidgetState extends State<CoordinateWidget> {
+  GoogleMapController _mapController;
+  final Map<String, Marker> _markers = {};
+  Position position;
+  Timer timer;
   @override
   Widget build(BuildContext context) {
     var coordProvider = Provider.of<CoordinateProviders>(context);
     var timerProvider = Provider.of<TimerProvider>(context);
-    var coordinate = coordProvider.coordinate;
+    position = coordProvider.coordinate;
+    timer = timerProvider.timer;
+    if (position.longitude != null || position.latitude != null) {
+      _showMarker();
+    }
     return Container(
       width: double.infinity,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text("Latitude: ${coordinate.latitude}", style: TextStyle(fontSize: 14),),
-          Text("Longitude: ${coordinate.longitude}", style: TextStyle(fontSize: 14),),
-          Text("Altitude: ${coordinate.altitude}", style: TextStyle(fontSize: 14),),
+          Container(
+            width: double.infinity,
+            height: 400,
+            child: GoogleMap(
+              myLocationButtonEnabled: false,
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(position.latitude ?? -6.914744, position.longitude ?? 107.609810),
+                zoom: 12
+              ),
+              markers: _markers.values.toSet(),
+            ),
+          ),
+          Text("Latitude: ${position.latitude ?? "Unknown"}", style: TextStyle(fontSize: 14),),
+          Text("Longitude: ${position.longitude ?? "Unknown"}", style: TextStyle(fontSize: 14),),
+          Text("Accuracy: ${position.accuracy ?? "Unknown"} meters", style: TextStyle(fontSize: 14),),
+          Text("Service: ${timer != null ? timer.isActive ? "Running" : "Not running" : "Not running"}"),
           SizedBox(height: 10,),
           Container(
-            width: 300,
+            width: 200,
             child: RaisedButton(
               onPressed: (){
                 coordProvider.fetchLocation();
@@ -46,7 +70,7 @@ class _CoordinateWidgetState extends State<CoordinateWidget> {
           ),
           SizedBox(height: 10,),
           Container(
-            width: 300,
+            width: 200,
             child: RaisedButton(
               onPressed: (){
                 timerProvider.startTimer();
@@ -62,7 +86,7 @@ class _CoordinateWidgetState extends State<CoordinateWidget> {
           ),
           SizedBox(height: 10,),
           Container(
-            width: 300,
+            width: 200,
             child: RaisedButton(
               onPressed: (){
                 timerProvider.stopTimer();
@@ -79,5 +103,32 @@ class _CoordinateWidgetState extends State<CoordinateWidget> {
         ],
       ),
     );
+  }
+
+  void _onMapCreated(GoogleMapController controller) async {
+    _mapController = controller;
+  }
+
+  void _showMarker() async {
+    setState(() async {
+      var myLoc = LatLng(position.latitude, position.longitude);
+      _mapController.animateCamera(CameraUpdate.newLatLngZoom(myLoc, 12));
+      _mapController.animateCamera(CameraUpdate.newLatLngZoom(myLoc, 17));
+      _markers.clear();
+      
+      // Marker can more than 1
+      final marker = Marker(
+        markerId: MarkerId("curloc"),
+        position: myLoc,
+        infoWindow: InfoWindow(
+          title: "Your location is here!",
+          snippet: "Hello",
+          onTap: (){
+            print("Hello you tapped your location!");
+          }
+        )
+      );
+      _markers['curloc'] = marker;
+    });
   }
 }
